4ebcb2cb3c8262b3a975e6b32c00e32a5fe70763
jerabekjak <jerabekjaku@gmail.com>
2019-09-20 17:33:49 +0200

    setup remote server and send the line reading

--- rpi-logger.py
+++ rpi-logger.py
@@ -21,26 +21,33 @@ class Humid(object):
 class Logger(object):
     """ Obstara vse okolo logovani """
 
-    def __init__ (self, dht_pin):
+    def __init__ (self, dht_pin, server, remote_dir):
         """ nastavy jednotliva cidla
         
         dht.pin: gpio na kterem je dht11
         """
 
+        ### GENERAL SETTINGS ###
         # nastaveni pracovniho adresare
         self._root = os.path.dirname(os.path.abspath(__file__))
-        print ('working directory in {}'.format(self._root))
-
-        # vytvoreni souboru na logovani
+        # current time
         nf = datetime.datetime.now()
+        # vytvoreni local souboru na logovani
         self._logfile = '{}/{}.dat'.format(self._root,nf.strftime('%Y%m%d%H%M'))
-        
+        # setup remote server
+        self._server = server
+        # setup remote soubor na logovani
+        self._remote_file = '{}/{}.dat'.format(remote_dir,nf.strftime('%Y%m%d%H%M'))
+
+        ### SETUP PROBES ###
         # nastavi dht11
         self._humid = Humid(dht_pin)
 
     def _send_reading(self,line):
         # TODO  pridat line do mycmd
-        mycmd = "ssh skola 'echo tesasdfasdft >> /home/jakub/public_html/test'"
+        mycmd = "ssh {} 'echo {} >> {}'".format(self._server, line, 
+                self._remote_file)
+        # print (mycmd)
         os.system(mycmd)
 
     def loop(self):
@@ -57,14 +64,17 @@ class Logger(object):
             line += str(ht[0]) + ' '
             line += str(ht[1])
             
+            self._send_reading(line)
             line += '\n'
             print (line)
-            with open(self._logfile,'a') as f:
-                f.write(line)
+
+            # with open(self._logfile,'a') as f:
+            #     f.write(line)
 
 
 if __name__ == '__main__':
 
-    logger = Logger(dht_pin=4)
+    logger = Logger(dht_pin=4, server='skola', 
+            remote_dir='/home/jakub/public_html/rpidatapeklo/')
     logger.loop()
 
